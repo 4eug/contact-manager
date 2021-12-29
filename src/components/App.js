dd41c61a7f3f31b3804e8f465ab5683be45c40c3
4eug <debraheug@gmail.com>
2021-12-29 19:46:22 +0000

    Adding Contacts

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList';
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]); 
 
-  const contacts = [
-    {
-      id: "1",
-      name: "John",
-      email: "johnceen@gmail.com",	
-    },
-    {
-      id: "2",
-      name: "Ben",
-      email: "Ben10@gmail.com",	
-    }
-  ];
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, contact]);
+  };
+
+  useEffect(() => {
+  const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+  if (retrieveContacts) setContacts(retrieveContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
   return (
     <div className='ui container'>
       <Header/>
-      <AddContact/>
+      <AddContact addContactHandler={addContactHandler}/>
       <ContactList contacts={contacts}/>
       </div>
   );
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -3,9 +3,12 @@ import ContactCard from "./ContactCard";
 
 const ContactList = (props) => {
 
+    const deleteContactHandler = (id) => {
+        props.getContact(id);
+    };
     const renderContactList = props.contacts.map((contact) => {
         return (
-           <ContactCard contact={contact}></ContactCard>
+           <ContactCard contact={contact} clickHandler = {deleteContactHandler}/>
         );
     });
     return (
